7cbc6a502631759ad770b6bfc9e989db67a076de
Jhohandrisapasuru <146504114+Jhohandrisapasuru@users.noreply.github.com>
2023-10-07 01:06:53 -0700

    Megalodon

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('tentang_kami', 'Home::tentang_kami');
+$routes->get('kontak', 'Home::kontak');
+$routes->get('kontak/(any)/(:num)', 'Home::kontak/$1/$2');
+$routes->get('tentang_kami/(any)/(:num)', 'Home::tentang_kami/$1/$2');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,12 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+    public function tentang_kami($parameter1,$parameter2): string
+    {
+        return "ini adalah parameter 1 : ". $parameter1. " dan ini adalah parameter 2 " .$parameter2;
+    }
+    public function kontak($parameter1,$parameter2): string
+    {
+        return "ini adalah parameter 1 : ". $parameter1. " dan ini adalah parameter 2 " .$parameter2;
+    }   
 }
